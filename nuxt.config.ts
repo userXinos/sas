af1ccdf61aefdb2d178a304b9b1bbef2127a7361
Xinos <userxinos@gmail.com>
2024-10-23 00:17:59 +0700

    basePath

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const isDev = process.env.NODE_ENV === 'development';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
   ssr: false,
+  app: {
+    baseURL: (isDev ? '/' : `/sas`),
+  },
 });
